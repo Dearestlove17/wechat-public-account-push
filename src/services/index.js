96b4e1f600b5c0ddeac2a31d8a98fca00de2caa0
Dearestlove17 <114079466+Dearestlove17@users.noreply.github.com>
2022-09-21 22:18:50 +0800

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -118,9 +118,8 @@ import dayjs from 'dayjs'
         
         if (nextBir === 0) {
             birthdayMessage = `今天是${birthday.name}生日哦，祝${birthday.name}生日快乐！`
-        } else  {
-            birthdayMessage = `距离${birthday.name}的生日还有${nextBir}天`
-        }
+        } else if(nextBir < 0) {nextBir=365+nextBir;birthdayMessage = `距离${birthday.name}的生日还有${nextBir}天`}
+           if (nextBir > 0 ) {birthdayMessage = `距离${birthday.name}的生日还有${nextBir}天`}
         // 存储数据
         if (birthdayMessage) {
             resMessage += `${birthdayMessage} \n`
